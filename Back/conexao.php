b36239abeb7c9ced4d3b7c880f1182f219b2017d
eduardoG <Eduardoeastro2@gmail.com>
2025-09-24 21:13:28 -0300

    conexao_php+dados

---
+++ Back/conexao.php
@@ -0,0 +1,11 @@
+<?php
+    $servidor="localhost";
+    $usuario="root";
+    $senha= "";
+    $bdprincipal= "studypack";
+
+    $conexao = mysqli_connect($servidor, $usuario, $senha, $bdprincipal);
+    if(!$conexao){
+        die("Deu probrema patrão, se liga ai-->" . mysqli_connect_error());
+    } 
+?>
